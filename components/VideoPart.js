dfa7e624bf1026e355697f89885ba0752890ba7a
LLTae2 <taehyun5820@gmail.com>
2022-09-14 20:14:08 +0900

    영상 아래 선택옵션

--- components/VideoPart.js
+++ components/VideoPart.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import styles from "../styles/VideoPart.module.css"
+import OptionBox from './OptionBox'
 
 const VideoPart = () => {
   return (
     <>
         <div className={styles.Video}></div>
         <div className={styles.Video_info_box}>
-            <div>
-                <span></span>
-                <p></p>
+            <div className={styles.Video_title_box}>
+                <span>#SeeYouAgain</span>
+                <p>Wiz Khalifa - See You Again ft. Charlie Puth [Official Video] Furious 7 Soundtrack</p>
             </div>
+            <div className={styles.Video_option}>
+                <p>조회수 805675회 • 2022. 3. 25.</p>
+                <div className={styles.Box_in_option}>
+                    <OptionBox />
+                </div>
+            </div>
+            <hr className={styles.Line_under_option} />
+            <div></div>
         </div>
     </>
   )

1f3e5d71b827230c2bdbf877fb9fcb0e1a7c3f6b
LLTae2 <taehyun5820@gmail.com>
2022-09-29 19:57:47 +0900

    file upload 부분에서 context 써서 파일간의 데이터 이동 가능

---
+++ components/FileInput.js
@@ -0,0 +1,76 @@
+import React, { useState, useContext } from 'react'
+import { FileUploader } from 'react-drag-drop-files'
+import styles from "../styles/FileInput.module.css"
+import { UploadStatus } from './Header';
+
+const FileInput = () => {
+    const [isTypeErr, setIsTypeErr] = useState(false);
+    const [onFileUploaded, setonFileUploaded] = useState(false);
+    const status = useContext(UploadStatus)
+
+    const handleChange = (file) => {
+        status.file = file;
+        console.log(status.file)
+        setonFileUploaded(true)
+    }
+    return (
+        <>
+            <div className={styles.Popup_top_box}>
+                <span>동영상 업로드</span>
+            </div>
+            <div className={styles.Popup_middle_box}>
+                <FileUploader
+                    handleChange={handleChange} 
+                    types={status.fileTypes}
+                    children
+                    name='input_file'
+                    onTypeError={() => {
+                        setIsTypeErr(true)
+                        setonFileUploaded(false)
+                    }}
+                    onDrop={() => {
+                        setIsTypeErr(false)
+                        setonFileUploaded(true)
+                        // setFile(e.target.value)
+                        // console.log(file)
+                    }}
+                    onSelect={() => {
+                        setIsTypeErr(false)
+                        setonFileUploaded(true)
+                        // setFile(e.target.value)
+                        // console.log(file)
+                    }}
+                >
+                    {
+                        isTypeErr ? <div style={{color: "red"}}>파일형식을 확인해주세요</div> 
+                        : onFileUploaded ? <div style={{color: "blue"}}>파일업로드 성공</div> 
+                        : <div>Drop here...</div>
+                    }
+                </FileUploader>
+                <p>동영상 파일을 드래그 앤 드롭하여 업로드(JPG, PNG, GIF)</p>
+                <span>동영상을 게시하기 전에는 비공개로 설정됩니다.</span>
+                <label 
+                    className={styles.File_submit_label} 
+                    htmlFor='input_file'
+                >파일 선택</label>
+                <input 
+                    id='input_file' 
+                    accept='image/*'
+                    style={{display: "none"}}   
+                    type="file"
+                    onChange={(e) => {
+                        status.file = e.target.files[0]
+                        setonFileUploaded(true)
+                        console.log(status.file)
+                    }}
+                />
+            </div>
+            <div className={styles.Popup_bottom_box}>
+                <p>YouTube에 동영상을 제출하면 YouTube 서비스 약관 및 커뮤니티 가이드에 동의하게 됩니다.</p>
+                <p>불법촬영물 게재시 삭제 조치되고 관련 법에 따라 처벌 받을 수 있습니다. 타인의 저작권 또는 개인정보 보호 권한을 침해해서는 안 됩니다.</p>
+            </div>
+        </>
+    )
+}
+
+export default FileInput
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>YouTube</title>
+        <link rel="icon" type="image/png" sizes="16x16" href='../../youtube.png'></link>
+      </Head>
       <Component {...pageProps}/>
       {/* <Header /> */}
     </>

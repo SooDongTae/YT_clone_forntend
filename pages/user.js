512f6eaab1f8710160f4fd19218b8c52e5f1982d
LLTae2 <taehyun5820@gmail.com>
2022-10-15 13:10:55 +0900

    Update::Update User Page

--- pages/user.js
+++ pages/user.js
@@ -17,12 +17,16 @@ const user = () => {
     <div className={styles.container}>
       <div id={styles.Profile} className={isOpened ? styles.user_info_part_show : styles.user_info_part}>
         <div>
-          <div>
-            <img src='https://yt3.ggpht.com/ytc/AMLnZu8rQyE9zHBy5XB7Y3vmnHXHHHBFdisJb_FWhg6J=s176-c-k-c0x00ffffff-no-rj' alt='' />
+          <div className={styles.profile_fl_box}>
             <div>
-              <p>코딩애플</p>
-              <span>구독자 {}</span>
+              <img src='https://yt3.ggpht.com/ytc/AMLnZu8rQyE9zHBy5XB7Y3vmnHXHHHBFdisJb_FWhg6J=s176-c-k-c0x00ffffff-no-rj' alt='' />
+              <div className={styles.name_fl_box}>
+                <p>코딩애플</p>
+                <span>구독자 10.4명</span>
+              </div>
             </div>
+            <div className={styles.sub_button}>구독</div>
+
           </div>
           <div>
 
